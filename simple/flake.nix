c78faea5235732fab0d33bed0005fd0586557925
uf5 <notbailey13@gmail.com>
2023-09-02 16:52:25 +0300

    Add the "simple" flake

---
+++ simple/flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in with pkgs; {
+        devShells.default = mkShell {
+          buildInputs = with pkgs;
+            [
+              # put packages here
+            ];
+        };
+      });
+}
